60cd03f710cb5b0c5753c5e8c687e755ef5c19b2
Fluke <iflukej@gmail.com>
2018-02-20 01:10:33 +0700

    Code

---
+++ Fluke/app/src/main/java/com/fluke/fluke/MainActivity.java
@@ -0,0 +1,34 @@
+package com.fluke.fluke;
+        import android.annotation.SuppressLint;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ViewFlipper;
+
+
+@SuppressLint("Registered")
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ViewFlipper viewFlipper;
+    Button next;
+    Button previous;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        next = (Button) findViewById(R.id.next);
+        previous = (Button) findViewById(R.id.previous);
+        next.setOnClickListener(this);
+        previous.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == next) {
+            viewFlipper.showNext();
+        }
+        else if (v == previous) {
+            viewFlipper.showPrevious();
+        }
+    }
+}
